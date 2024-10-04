fb31483e3b215e807d37d391f16bb3365496393f
morr33 <gborieampomah@gmail.com>
2024-10-03 18:36:29 -0700

    main

---
+++ 0x00-pascal_triangle/1-main.py
@@ -0,0 +1,15 @@
+Python
+from your_file import pascal_triangle
+
+def print_triangle(triangle):
+    """
+    Print the triangle
+    """
+    for row in triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))
+
+if __name__   
+ == "__main__":
+    n = 5
+    triangle = pascal_triangle(n)
+    print_triangle(triangle)
